2f46122b0e36c0b4f2aaecbbfb083bdf3744ff90
Ilya Shaisultanov <ilya.shaisultanov@gmail.com>
2019-02-20 06:47:13 -0800

    Add a file endpoint

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 const http = require('http')
 const os = require('os')
+const url = require('url')
+const cp = require('child_process')
+const fs = require('fs')
 const id = require('crypto').randomBytes(8).toString('hex')
 
 const interfaces = os.networkInterfaces()
@@ -22,4 +25,22 @@ http.createServer(function(req, res) {
   res.end(`${JSON.stringify({ name: 'extra', id: id, time: new Date() })}\n`)
 }).listen(4000)
 
+
+http.createServer(function (req, res) {
+  const q = url.parse(req.url, true)
+
+  let size = '100M'
+
+  if (q && q.query && q.query.size) size = q.query.size
+
+  const filename = `file-${size}`
+
+  try {
+    const stat = fs.statSync(filename)
+  } catch (error) {
+    cp.spawnSync('truncate', ['-s' , size, `file-${size}`])
+  }
+
+  fs.createReadStream(filename).pipe(res)
+}).listen(5000)
 
